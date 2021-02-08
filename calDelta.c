d07538267498af9874f611c2d6d3d5bc91d28511
Aparecida <moretoap4@gmail.com>
2021-02-08 10:19:45 -0300

    calRaiz

--- calDelta.c
+++ calDelta.c
@@ -1,3 +1,6 @@
+/*
+Atividade-2: Raízes de equação de segundo grau
+*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
@@ -5,12 +8,12 @@
 int main()
 {
 	float delta, a, b, c, raiz1, raiz2;
-	printf("Calculo do delta");
+	printf("Calculo das Raizes da equacao de segundo grau\n");
 	
 	printf("Digite o valor de 'a': \n");
 	scanf("%f",&a);
 	if (a == 0){
-		printf("EQUACAO NAO É DE SEGUNDO GRAU.");}
+		printf("EQUACAO NAO Eh DE SEGUNDO GRAU.");}
 	printf("Digite o valor de 'b': \n");
 	scanf("%f",&b);
 	
@@ -20,19 +23,20 @@ int main()
 	delta =  ( b * b ) - (4 *  a * c);
 	
 		
-
 	if (delta < 0) {
-		printf("\nDelta = %f (Delta < 0): Raizes nao pertence ao conjunto dos numeros reais. ", delta);		
+		printf("\nDelta = %.2f (Delta < 0): Raizes nao pertence ao conjunto dos numeros reais. ", delta);		
 		}
 			
 	else if (delta > 0) {
 		raiz1 = ((-b) + sqrt(delta))/(2 * (a));
 		raiz2 = ((-b) - sqrt(delta))/(2 * (a));
-		printf("\nDelta = %f (Delta > 0)\nValor da raiz 1 = %.2f\nValor da raiz 2 = %.2f", delta, raiz1, raiz2);
+		printf("\nDelta = %.2f (Delta > 0)\nValor da raiz 1 = %.2f\nValor da raiz 2 = %.2f", delta, raiz1, raiz2);
 		}	
-	else 
-		printf("\nDelta = %f (Delta = 0): A funcao possui duas raiz iguais.\n Raiz = %.2f ", delta, raiz1);
-	
+	else {
+		raiz1 = ((-b) + sqrt(delta))/(2 * (a));
+		printf("\nDelta = %.2f (Delta = 0): A funcao possui duas raiz iguais.\n Raiz = %.2f ", delta, raiz1);
+		}
+
 	return 0;
 }
 		
